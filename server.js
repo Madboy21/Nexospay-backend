14cc24194d24e911f5bab8b0d0598601e9cd1532
Madboy21 <justinjordan2211@gmail.com>
2025-08-14 20:17:18 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
-
 import userRoutes from "./routes/userRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
-import withdrawRoutes from "./routes/withdrawRoutes.js";
-import bot from "./bot.js";
 
 dotenv.config();
-const app = express();
+connectDB();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
-app.use("/api/withdraw", withdrawRoutes);
-
-// Connect DB
-connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
