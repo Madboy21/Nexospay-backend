20a76fc05b57045f7622151ce5971de9a1a9787b
Madboy21 <justinjordan2211@gmail.com>
2025-08-14 19:30:51 +0600

    bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,7 @@
+import TelegramBot from "node-telegram-bot-api";
+import dotenv from "dotenv";
+dotenv.config();
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+export default bot;
 
