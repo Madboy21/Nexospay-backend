233e0a6635e007dae925ce1e6b7581ee06914f4b
Madboy21 <justinjordan2211@gmail.com>
2025-08-14 20:14:14 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -7,20 +7,12 @@ const userSchema = new mongoose.Schema({
   lastName: String,
   tokens: { type: Number, default: 0 },
   completedTasks: { type: Number, default: 0 },
+  tasksToday: { type: Number, default: 0 },
+  dayStamp: String,
   referrals: { type: Number, default: 0 },
   referralBonusTokens: { type: Number, default: 0 },
-  referredBy: { type: String, default: null },
-  dayStamp: { type: String, default: null },
-  tasksToday: { type: Number, default: 0 },
-  withdrawRequests: [
-    {
-      amount: Number,
-      date: { type: Date, default: Date.now },
-      status: { type: String, enum: ["pending","approved","rejected"], default: "pending" }
-    }
-  ]
-}, { timestamps: true });
+  referredBy: String,
+  withdrawRequests: Array
+});
 
 export default mongoose.model("User", userSchema);
-
-
