1e5c6c9336c3680d3e408829892b96bc74393ea4
Madboy21 <justinjordan2211@gmail.com>
2025-08-14 18:47:03 +0600

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+import userRoutes from "./routes/userRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -10,9 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/tasks', require('./routes/taskRoutes'));
-app.use('/api/withdraw', require('./routes/withdrawRoutes'));
+app.use("/api/users", userRoutes);
+app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, ()=> console.log(`🚀 Server running on port ${PORT}`));
